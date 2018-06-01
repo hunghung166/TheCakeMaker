aa0d19334bf9dacd80891d740634d5276fc0573c
hunghung166 <hungnvd00436@fpt.edu.vn>
2018-06-02 00:43:53 +0700

    Add project files.

---
+++ TheCakeMaker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheCakeMaker.MvcApplication" Language="C#" %>
---
+++ TheCakeMaker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheCakeMaker.Startup))]
+namespace TheCakeMaker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
